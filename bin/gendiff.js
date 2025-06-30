a68a200d6fd553770f8f278a79107adadffe771a
Oksana Auto-QA <oksana.zhornova@gmail.com>
2025-06-30 19:17:13 +0500

    feat(cli): exact help output; remove default format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,6 +11,14 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version(version, '-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'display help for command')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format')
+  .helpOption('-h, --help', 'output usage information')
+
+  .action((filepath1, filepath2, options) => {
+    console.log(filepath1, filepath2, options.format)
+  })
+  
 
 program.parse()
